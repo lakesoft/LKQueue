673e6afd6787ba46ec3396ca0376e146f5765114
Hiroshi Hashiguchi <xcatsan@mac.com>
2011-05-10 12:13:04 +0900

    workset 2011-05-10(2)

--- TestApplication/RootViewController.h
+++ TestApplication/RootViewController.h
@@ -1,10 +1,4 @@
-//
 //  RootViewController.h
-//  FBQueue
-//
-//  Created by Hiroshi Hashiguchi on 11/04/12.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
-//
 
 #import <UIKit/UIKit.h>
 
